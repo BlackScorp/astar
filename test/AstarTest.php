a6f9899b5d42844f60314ab652490c03df284ffb
BlackScorp <witalimik@web.de>
2015-03-11 16:26:33 +0100

    removed tests which doesn't affect the code coverage

--- test/AstarTest.php
+++ test/AstarTest.php
@@ -43,25 +43,6 @@ class AstarTest extends PHPUnit_Framework_TestCase
         $this->assertEmpty($result);
     }
 
-    public function testRightBottomPath()
-    {
-        $map = [
-            [1, 1, 1, 1, 1],
-            [1, 0, 0, 0, 1],
-            [1, 0, 0, 0, 1],
-            [1, 0, 0, 0, 1],
-            [1, 1, 1, 1, 1],
-        ];
-        $graph = new \BlackScorp\Astar\Graph($map);
-        $start_node = $graph->node(1, 1);
-        $end_node = $graph->node(2, 2);
-        $astar = new \BlackScorp\Astar\Astar($graph);
-        $astar->blocked(array(1));
-
-        $result = $astar->search($start_node, $end_node);
-        $this->assertSame(2, count($result));
-    }
-
     public function testDiagonalPath()
     {
         $map = [
@@ -79,63 +60,6 @@ class AstarTest extends PHPUnit_Framework_TestCase
         $astar->diagonal(true);
         $result = $astar->search($start_node, $end_node);
         $this->assertSame(1, count($result));
-    }
-
-    public function testComplexDiagonalPath()
-    {
-        $map = [
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0],
-            [0, 0, 1, 0, 0],
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0],
-        ];
-        $graph = new \BlackScorp\Astar\Graph($map);
-        $start_node = $graph->node(0, 0);
-        $end_node = $graph->node(4, 4);
-        $astar = new \BlackScorp\Astar\Astar($graph);
-        $astar->blocked(array(1));
-        $astar->diagonal(true);
-        $result = $astar->search($start_node, $end_node);
-        $this->assertSame(5, count($result));
-    }
-
-    public function testComplexPath()
-    {
-        $map = [
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0],
-            [0, 0, 1, 0, 0],
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0],
-        ];
-        $graph = new \BlackScorp\Astar\Graph($map);
-        $start_node = $graph->node(0, 0);
-        $end_node = $graph->node(4, 4);
-        $astar = new \BlackScorp\Astar\Astar($graph);
-        $astar->blocked(array(1));
-
-        $result = $astar->search($start_node, $end_node);
-        $this->assertSame(8, count($result));
-    }
-
-    public function testPathWithCosts()
-    {
-        $map = [
-            [0, 0, 0, 0, 0],
-            [4, 1, 0, 1, 0],
-            [0, 0, 0, 1, 0],
-            [0, 1, 0, 0, 0],
-            [0, 1, 0, 0, 1],
-        ];
-        $graph = new \BlackScorp\Astar\Graph($map);
-        $start_node = $graph->node(0, 0);
-        $end_node = $graph->node(2, 0);
-        $astar = new \BlackScorp\Astar\Astar($graph);
-        $astar->blocked(array(1));
-        $result = $astar->search($start_node, $end_node);
-
-        $this->assertSame(6, count($result));
     }
 
     public function testDiagonalHeursitic()
